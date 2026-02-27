ee09b7dbc85972c650077b1d75a1826f1f8a94e9
Kreszen Vallentino Arjuna Wijono <echenwijono@gmail.com>
2026-02-27 10:26:53 +0700

    PWL_Jobsheet2_Praktikum1

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,34 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/hello', function () {
+    return ('Hello World');
+});
+
+Route::get('/world', function () {
+    return 'World';
+});
+
+Route::get('/selamat', function () {
+    return 'selamat datang';
+});
+
+Route::get('/about', function () {
+    return '244107020206 | Kreszen Vallentino Arjuna Wijono';
+});
+
+Route::get('/user/{name}', function ($name) {
+    return 'Nama saya '.$name;
+});
+
+Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
+    return 'Pos ke-'.$postId." Komentar ke: ".$commentId;
+});
+
+Route::get('/articles/{id}', function ($id) {
+    return "Halaman Artikel dengan ID ".$id;
+});
+
+Route::get('/user/{name?}', function ($name='John') {
+    return 'Nama saya '.$name;
 });
