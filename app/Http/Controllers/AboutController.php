12a8b9c93539d8253c5b7f20135c12bd7e24064a
Kreszen Vallentino Arjuna Wijono <echenwijono@gmail.com>
2026-02-28 15:15:47 +0700

    PWL_Jobsheet2_Praktikum2

---
+++ app/Http/Controllers/AboutController.php
@@ -0,0 +1,10 @@
+<?php
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+
+class AboutController extends Controller
+{
+   public function about() {
+    return 'Nama: Kreszen Vallentino Arjuna Wijono, NIM: 244107020206';
+}
+}
--- routes/web.php
+++ routes/web.php
@@ -2,34 +2,60 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/hello', function () {
-    return ('Hello World');
-});
+use App\Http\Controllers\WelcomeController;
 
-Route::get('/world', function () {
-    return 'World';
-});
+use App\Http\Controllers\PageController;
 
-Route::get('/selamat', function () {
-    return 'selamat datang';
-});
+// Route::get('/', [PageController::class, 'index']);
+// Route::get('/about', [PageController::class, 'about']);
+// Route::get('/articles/{id}', [PageController::class, 'articles']);
 
-Route::get('/about', function () {
-    return '244107020206 | Kreszen Vallentino Arjuna Wijono';
-});
+// Route::get('/hello', [WelcomeController::class, 'hello']);
 
-Route::get('/user/{name}', function ($name) {
-    return 'Nama saya '.$name;
-});
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\ArticleController;
 
-Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
-    return 'Pos ke-'.$postId." Komentar ke: ".$commentId;
-});
+Route::get('/', [HomeController::class, 'index']);
+Route::get('/about', [AboutController::class, 'about']);
+Route::get('/articles/{id}', [ArticleController::class, 'articles']);
 
-Route::get('/articles/{id}', function ($id) {
-    return "Halaman Artikel dengan ID ".$id;
-});
+// Route::get('/hello', function () {
+//     return ('Hello World');
+// });
 
-Route::get('/user/{name?}', function ($name='John') {
-    return 'Nama saya '.$name;
-});
+// Route::get('/world', function () {
+//     return 'World';
+// });
+
+// Route::get('/selamat', function () {
+//     return 'selamat datang';
+// });
+
+// Route::get('/about', function () {
+//     return '244107020206 | Kreszen Vallentino Arjuna Wijono';
+// });
+
+// Route::get('/user/{name}', function ($name) {
+//     return 'Nama saya '.$name;
+// });
+
+// Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
+//     return 'Pos ke-'.$postId." Komentar ke: ".$commentId;
+// });
+
+// Route::get('/articles/{id}', function ($id) {
+//     return "Halaman Artikel dengan ID ".$id;
+// });
+
+// Route::get('/user/{name?}', function ($name='John') {
+//     return 'Nama saya '.$name;
+// });
+
+use App\Http\Controllers\PhotoController;
+
+// Route::resource('photos', PhotoController::class);
+
+Route::resource('photos', PhotoController::class)->only([
+    'index', 'show'
+]);
